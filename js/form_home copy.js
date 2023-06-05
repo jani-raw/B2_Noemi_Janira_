3c60b8a99a8dce0158ff037d2eedfd5f5d8b7e05
jani-raw <132079309+jani-raw@users.noreply.github.com>
2023-06-05 15:19:40 +0200

    Add files via upload

--- js/form_home copy.js
+++ js/form_home copy.js
@@ -1,16 +1,20 @@
 
-
 // (1) Variablen initialisieren
 const formContainer = document.getElementById("formContainer");
 const gameContainer = document.getElementById("game-container");
 const submitButton = document.getElementById("submit");
 submitButton.disabled = true;
+const vornameField = document.getElementById("vorname");
+const nachnameField = document.getElementById("nachname");
 const emailField = document.getElementById("email");
+const infoField = document.getElementById("info");
+
 
 // (2) Interaktionen festlegen
 emailField.addEventListener("keyup", () => {
   onChangeEmailField();
 });
+
 submitButton.addEventListener("click", async (event) => {
   event.preventDefault();
   onClickSubmit();
@@ -29,31 +33,22 @@ const onClickSubmit = async () => {
   const data = {
     group: "b2", // SQL Gruppen Namen
     pw: "aa61f63b", // SQL Passwort
-    tableName: "game", // Name der Tabelle in der SQL Datenbank
+    tableName: "user", // Name der Tabelle in der SQL Datenbank
 
     columns: {
       // "email" Name der Spalte in der SQL Tabelle
       // "emailField.value" Eingabe des Benutzers aus dem Formularfeld
+      vorname: vornameField.value,
+      nachname: nachnameField.value,
       email: emailField.value,
+      info: infoField.value
+
+
     },
+  
   };
   // Speichert die Daten in der Datenbank
   await databaseClient.insertInto(data);
 
-  // Nach dem Speichern verschwindet das Formular, das Game erscheint
-  formContainer.classList.add("hidden");
-  gameContainer.classList.remove("hidden");
-};
+;}
 
-
-//Validierung vom Formular
-function validateemail()  
-{  
-var x=document.myformtwo.email.value;  
-var atposition=x.indexOf("@");  
-var dotposition=x.lastIndexOf(".");  
-if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-  alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
-  return false;  
-  }  
-}  
