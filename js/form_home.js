e15ba0936cfb0bae21b10701a1c42a44f20f8406
jani-raw <132079309+jani-raw@users.noreply.github.com>
2023-06-11 20:59:15 +0200

    Add files via upload

--- js/form_home.js
+++ js/form_home.js
@@ -23,34 +23,40 @@ emailField.addEventListener("keyup", () => {
 phoneField.addEventListener("keyup", () => {
   validateform();
 });
+
 submitButton.addEventListener("click", async (event) => {
   event.preventDefault();
   onClickSubmit();
 });
 
 // (3) Interaktionen Code
-const validateform = () => {
-  if (emailField.value === "") {
-    submitButton.disabled = true;
-  } else {
+
+function validateform() {
+  const vornameValue = vornameField.value;
+  const nachnameValue = nachnameField.value;
+  const emailValue = emailField.value;
+  const phoneValue = phoneField.value;
+
+  if (vornameValue !== "" && nachnameValue !== "" && emailValue !== "" && phoneValue !== "") {
     submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
   }
-};
-const onClickSubmit = async () => {
+}
+async function onClickSubmit() {
   // Daten aus dem Formular für die Datenbank bereitstellen
   const data = {
-    group: "b2", // SQL Gruppen Namen
-    pw: "aa61f63b", // SQL Passwort
-    tableName: "user", // Name der Tabelle in der SQL Datenbank
+    group: "b2",
+    pw: "aa61f63b",
+    tableName: "newsletter",
 
     columns: {
       // "email" Name der Spalte in der SQL Tabelle
-      // "emailField.value" Eingabe des Benutzers aus dem Formularfeld
+      //emailField.value" Eingabe des Benutzers aus dem Formularfeld
       vorname: vornameField.value,
       nachname: nachnameField.value,
       email: emailField.value,
       phone: phoneField.value
-
     },
   };
   // Speichert die Daten in der Datenbank
@@ -59,82 +65,7 @@ const onClickSubmit = async () => {
   // Nach dem Speichern verschwindet das Formular, das Game erscheint
   formContainer.classList.add("hidden");
   gameContainer.classList.remove("hidden");
-};
-
-
-
-
-
-
-
-/*
-// (1) Variablen initialisieren
-const myform = document.getElementById("formularContainer");
-const gameContainer = document.getElementById("Thankyou");
-const submitButton = document.getElementById("submit");
-submitButton.disabled = true;
-const vornameField = document.getElementById("vorname");
-const nachnameField = document.getElementById("nachname");
-const emailField = document.getElementById("email");
-const phoneField = document.getElementById("phone");
-
-
-
-// (2) Interaktionen festlegen
-vornameField.addEventListener("keyup", () => {
-  validateForm();
-});
-
-nachnameField.addEventListener("keyup", () => {
-  validateForm();
-});
-
-emailField.addEventListener("keyup", () => {
-  validateForm();
-});
-
-phoneField.addEventListener("keyup", () => {
-  validateForm();
-});
-
-
-submitButton.addEventListener("click", async (event) => {
-  event.preventDefault();
-  onClickSubmit();
-});
-
-
-
-// (3) Interaktionen Code
-const validateForm = () => {
- // if (vornameField.value === "" || nachnameField.value === "")
-  if (vornameField === "" || nachnameField === "" || emailField === "" || phoneField === "" )  {
-    submitButton.disabled = true;
-  } else { 
-    submitButton.disabled = false; 
-  }
-};
-
-
-
-async function onClickSubmit() {
-  // Daten aus dem Formular für die Datenbank bereitstellen
-  const data = {
-    group: "b2",
-    pw: "aa61f63b",
-    tableName: "user",
-
-    columns: {
-      // "email" Name der Spalte in der SQL Tabelle
-      // "emailField.value" Eingabe des Benutzers aus dem Formularfeld
-      vorname: vornameField.value,
-      nachname: nachnameField.value,
-      email: emailField.value,
-      phone: phoneField.value,
-    },
-  };
-  // Speichert die Daten in der Datenbank
-  await databaseClient.insertInto(data);
-  ;
 }
-*/
+
+
+
